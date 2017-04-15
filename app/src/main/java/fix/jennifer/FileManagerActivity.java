325bb3f79b136f60b27b8548cc8a87a1bd2be002
Anatoly <fix@Anatolys-MacBook-Pro.local>
2017-04-15 20:03:30 +0300

    initial commit

---
+++ app/src/main/java/fix/jennifer/FileManagerActivity.java
@@ -0,0 +1,77 @@
+package fix.jennifer;
+
+/**
+ * Created by fix on 15.04.17.
+ */
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.*;
+
+public class FileManagerActivity extends AppCompatActivity {
+    private FilesAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_manager);
+      generateNoteOnSD(getApplicationContext(), "sss", "sss");
+
+        adapter = new FilesAdapter(this);
+       //  String user_path = getFilesDir().getAbsolutePath();
+    // Log.d("APP PATYH", user_path);
+        //  String user_path = (String)Environment.getRootDirectory();
+
+        String filename = "myfile";
+        String string = "Hello world!";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        adapter.setDirectory(new File(getFilesDir().getAbsolutePath()));
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.files);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!adapter.goBack()) {
+            super.onBackPressed();
+        }
+    }
+    public void generateNoteOnSD(Context context, String sFileName, String sBody) {
+        try {
+            String user_path = getFilesDir().getAbsolutePath();
+
+            File root = new File(user_path, "Notes");
+            if (!root.exists()) {
+                root.mkdirs();
+            }
+            File gpxfile = new File(root, sFileName);
+            FileWriter writer = new FileWriter(gpxfile);
+            writer.append(sBody);
+            writer.flush();
+            writer.close();
+            Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/fix/jennifer/FilesAdapter.java
@@ -0,0 +1,102 @@
+package fix.jennifer;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+/**
+ * Created by fix on 15.04.17.
+ */
+class FilesAdapter extends RecyclerView.Adapter<FilesAdapter.FileViewHolder> {
+    private final LayoutInflater layoutInflater;
+
+    private File[] files;
+    private File currentFile;
+
+    FilesAdapter(final Context context) {
+        layoutInflater = LayoutInflater.from(context);
+    }
+
+    void setDirectory(final File file) {
+        this.currentFile = file;
+        this.files = file.listFiles();
+        sortFiles(this.files);
+        notifyDataSetChanged();
+    }
+
+    boolean goBack() {
+        File parent = currentFile.getParentFile();
+        if (parent != null) {
+            setDirectory(parent);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public FileViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new FileViewHolder(layoutInflater.inflate(R.layout.file_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(FileViewHolder holder, int position) {
+        holder.bind(files[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return files != null ? files.length : 0;
+    }
+
+    private static void sortFiles(final File[] files) {
+        if (files == null) {
+            return;
+        }
+        Arrays.sort(files, new Comparator<File>() {
+            @Override
+            public int compare(File f1, File f2) {
+                if (f1.isDirectory() && !f2.isDirectory()) return -1;
+                if (f2.isDirectory() && !f1.isDirectory()) return 1;
+                return f1.getName().compareTo(f2.getName());
+            }
+        });
+    }
+
+    final class FileViewHolder extends RecyclerView.ViewHolder {
+        private final TextView filename;
+
+        FileViewHolder(View view) {
+            super(view);
+            filename = (TextView) view.findViewById(R.id.filename);
+        }
+
+        void bind(final File file) {
+            if (file.isDirectory()) {
+                filename.setTypeface(null, Typeface.BOLD);
+                filename.setText("[" + file.getName() + "]");
+            } else {
+                filename.setTypeface(null, Typeface.NORMAL);
+                filename.setText(file.getName());
+            }
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (file.isDirectory()) {
+                        FilesAdapter.this.setDirectory(file);
+                    }
+                }
+            });
+        }
+    }
+}
+
